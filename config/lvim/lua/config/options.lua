1b9952702fab3cb99025e99f793ce27f0f2fd9cb
suwapotta <nguyenducthientan09@gmail.com>
2026-05-20 18:06:37 +0700

    [ AUTO ] NixOS Generation 175
    Saved latest generation 175 at 2026-05-20 11:06:37.

--- config/lvim/lua/config/options.lua
+++ config/lvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Don't create swap files
+vim.opt.swapfile = false
+
 -- Rounded diagnostic border
 vim.diagnostic.config({
   float = {
