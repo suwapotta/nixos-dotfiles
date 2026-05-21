97f7730c0289014686eaf72afd5ad6bb2baa4db3
suwapotta <nguyenducthientan09@gmail.com>
2026-05-21 22:54:08 +0700

    Legacy VM NixOS image
    Signed-off-by: suwapotta <nguyenducthientan09@gmail.com>

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -4,9 +4,13 @@
 }:
 
 {
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    kernelPackages = pkgs.linuxPackages_latest;
   };
 
   users.users."vm-user" = {
@@ -15,9 +19,7 @@
     initialHashedPassword = "$y$j9T$qgB61P4hN.2sCf4gUbjH11$le712A/0qeCUyOICLBe7NxSfgNB4hslcpFkxNXHA4a7";
   };
 
-  environment.systemPackages = with pkgs; [
-    neovim
-  ];
+  environment.systemPackages = with pkgs; [ fastfetch ];
 
   system.stateVersion = "25.11";
 }
