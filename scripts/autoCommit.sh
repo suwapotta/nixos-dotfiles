81299ae83483b63858411053741ea730cd86725a
suwapotta <nguyenducthientan09@gmail.com>
2026-05-22 06:17:48 +0700

    [ AUTO ] NixOS Generation 176
    Saved latest generation 176 at 2026-05-21 23:17:48.

--- scripts/autoCommit.sh
+++ scripts/autoCommit.sh
@@ -5,13 +5,13 @@ MESS="[ AUTO ] NixOS Generation $GEN"
 DESC="Saved latest generation $GEN at $(date -u +%Y-%m-%d\ %H:%M:%S)."
 
 C_GREEN="\033[1;32m"
-C_BLUE="\033[1;34m"
+# C_BLUE="\033[1;34m"
 C_NONE="\033[0m"
 
-if git diff --quiet --cached; then
-  printf "%b   SKIPPED  %b Auto-commit\n" "${C_BLUE}" "${C_NONE}"
-  exit 0
-fi
+# if git diff --quiet --cached; then
+#   printf "%b   SKIPPED  %b Auto-commit\n" "${C_BLUE}" "${C_NONE}"
+#   exit 0
+# fi
 
 git add -A "$HOME/nixos-dotfiles"
 git commit -m "$MESS" -m "$DESC" &>/dev/null
