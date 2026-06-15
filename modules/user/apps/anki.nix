f1c0417e893d8a1e170f7ae5e22c5d689a431f3f
suwapotta <nguyenducthientan09@gmail.com>
2026-06-15 18:56:11 +0700

    Revive anki with catppuccin theme
    Signed-off-by: suwapotta <nguyenducthientan09@gmail.com>

--- modules/user/apps/anki.nix
+++ modules/user/apps/anki.nix
@@ -1,17 +1,51 @@
 {
   flake.homeModules."anki" =
-    { pkgs, ... }:
+    {
+      pkgs,
+      ...
+    }:
 
     {
-      home.packages = [
-        (pkgs.anki.withAddons (
-          with pkgs.ankiAddons;
-          [
-            anki-connect
-            review-heatmap
-            ajt-card-management
-          ]
-        ))
-      ];
+      programs.anki = {
+        enable = true;
+        addons = with pkgs.ankiAddons; [
+          anki-connect
+          review-heatmap
+          ajt-card-management
+
+          # NOTE: When add-on is not available in nixpkgs
+          # (pkgs.anki-utils.buildAnkiAddon (finalAttrs: {
+          #   pname = "recolor";
+          #   version = "3.1";
+          #   src = pkgs.fetchFromGitHub {
+          #     owner = "AnKing-VIP";
+          #     repo = "AnkiRecolor";
+          #     rev = finalAttrs.version;
+          #     sparseCheckout = [ "src/addon" ];
+          #     hash = "sha256-28DJq2l9DP8O6OsbNQCZ0pm4S6CQ3Yz0Vfvlj+iQw8Y=";
+          #   };
+          #   sourceRoot = "${finalAttrs.src.name}/src/addon";
+          # }))
+        ];
+
+        answerKeys = [
+          {
+            ease = 1;
+            key = "h";
+          }
+          {
+            ease = 2;
+            key = "j";
+          }
+          {
+            ease = 3;
+            key = "k";
+          }
+          {
+            ease = 4;
+            key = "l";
+          }
+        ];
+      };
     };
 }
