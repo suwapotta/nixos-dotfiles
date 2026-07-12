af08b68160e5b1e91701df099962f1471d17d139
suwapotta <nguyenducthientan09@gmail.com>
2026-07-12 15:24:57 +0700

    [ laptop ] NixOS Generation 218
    Saved latest generation 218 at 2026-07-12 08:24:57.

--- modules/core/hardware/msi.nix
+++ modules/core/hardware/msi.nix
@@ -55,15 +55,18 @@ in
         {
           eco = [
             "w /sys/devices/platform/msi-ec/webcam_block - - - - on"
-            "w /sys/devices/platform/msi-ec/leds/msiacpi::kbd_backlight/brightness - - - - 0"
+            "w /sys/class/leds/msiacpi::kbd_backlight - - - - 0"
+            "w /sys/devices/platform/msi-ec/cooler_boost - - - - off"
           ];
           comfort = [
             "w /sys/devices/platform/msi-ec/webcam_block - - - - off"
-            "w /sys/devices/platform/msi-ec/leds/msiacpi::kbd_backlight/brightness - - - - 3"
+            "w /sys/class/leds/msiacpi::kbd_backlight - - - - 3"
+            "w /sys/devices/platform/msi-ec/cooler_boost - - - - on"
           ];
           turbo = [
             "w /sys/devices/platform/msi-ec/webcam_block - - - - off"
-            "w /sys/devices/platform/msi-ec/leds/msiacpi::kbd_backlight/brightness - - - - 3"
+            "w /sys/class/leds/msiacpi::kbd_backlight - - - - 3"
+            "w /sys/devices/platform/msi-ec/cooler_boost - - - - on"
           ];
         }
         .${cfg.ec.preset}
