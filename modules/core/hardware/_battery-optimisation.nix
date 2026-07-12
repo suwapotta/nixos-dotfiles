15dee235acfc171126afde05966f3dd54883af6e
suwapotta <nguyenducthientan09@gmail.com>
2026-07-12 21:19:59 +0700

    Hidden module for battery optimisation
    Signed-off-by: suwapotta <nguyenducthientan09@gmail.com>

---
+++ modules/core/hardware/_battery-optimisation.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  config,
+  ...
+}:
+
+{
+  options = {
+    modules.core.hardware._battery-optimisation.enable = lib.mkEnableOption "battery optimisation";
+  };
+
+  config = lib.mkIf config.modules.core.hardware._battery-optimisation.enable {
+    systemd.tmpfiles.rules = [
+      "w /sys/devices/system/cpu/intel_pstate/max_perf_pct - - - - 50"
+    ];
+
+    boot = {
+      kernel.sysctl = {
+        "vm.dirty_writeback_centisecs" = 6000;
+      };
+
+      kernelParams = [
+        "i915.enable_fbc=1"
+        "i915.enable_psr=2"
+        "i915.enable_dc=4"
+        "i915.enable_guc=3"
+        "nmi_watchdog=0"
+      ];
+
+      extraModprobeConfig = ''
+        # Force audio chip to go to sleep after 1s
+        options snd_hda_intel power_save=1 power_save_controller=Y
+      '';
+    };
+  };
+}
--- modules/specialisation/travel.nix
+++ modules/specialisation/travel.nix
@@ -20,6 +20,7 @@
 
         core = {
           hardware = {
+            _battery-optimisation.enable = true;
             bluetooth.enable = lib.mkForce false;
             msi = {
               enable = lib.mkForce true;
@@ -43,30 +44,6 @@
             };
           };
         };
-      };
-
-      # Intel CPU/iGPU
-      systemd.tmpfiles.rules = [
-        "w /sys/devices/system/cpu/intel_pstate/max_perf_pct - - - - 50"
-      ];
-
-      boot = {
-        kernel.sysctl = {
-          "vm.dirty_writeback_centisecs" = 6000;
-        };
-
-        kernelParams = [
-          "i915.enable_fbc=1"
-          "i915.enable_psr=2"
-          "i915.enable_dc=4"
-          "i915.enable_guc=3"
-          "nmi_watchdog=0"
-        ];
-
-        extraModprobeConfig = ''
-          # Force audio chip to go to sleep after 1s
-          options snd_hda_intel power_save=1 power_save_controller=Y
-        '';
       };
     };
   };
